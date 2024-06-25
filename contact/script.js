e0360037db97455420f9db80732976d62fea213b
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-06-26 03:23:52 +0500

    first commit

---
+++ contact/script.js
@@ -0,0 +1,4 @@
+document.getElementById('send').addEventListener('click', function(){
+    // event.preventDefault();  
+    document.getElementById('success').innerHTML = 'Your message has been sent successfully';
+});
